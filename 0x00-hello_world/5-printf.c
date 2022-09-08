c9e14da3297b069607b9a37257cd79ccca2f0923
Paradox <fathiaraji4@gmail.com>
2022-09-08 12:21:25 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Entry point 
-* 
-* Description: prints "with proper grammar, but the outcome is a piece of 
-* art,
-*
-* Return: 0
-*/
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
