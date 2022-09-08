90d63865f983364fda578e586f56ae0750964577
Paradox <fathiaraji4@gmail.com>
2022-09-08 14:32:14 -0700

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,7 @@
  * Return: 0
  */
 int main(void)
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
+}
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(c));
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(li));
