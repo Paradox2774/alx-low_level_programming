2903150bceb26c8dcce272f31f404978598bc577
Paradox <fathiaraji4@gmail.com>
2022-09-08 10:14:33 -0700

    6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts function 
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle 
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+        printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -10,3 +10,5 @@ Task 4. Write a C program that prints exactly "Programming is like building a mu
 
 Task 5. Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
 
+Task 6. Write a C program that prints the size of various types on the computer it is compiled and run on.
+
