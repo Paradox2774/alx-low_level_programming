d54221607bcc824e520bc754edf2057c513f1b22
Paradox <fathiaraji4@gmail.com>
2022-09-08 12:49:33 -0700

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/**
- * main - print the string in the puts function 
+/** 
+ * main - print thee string in the put function
  *
  * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle 
- * return: 0
+ * this program prints "Programming is like building a multilingual puzzle 
+ * Return: 0
  */
 int main(void)
 {
@@ -15,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-        printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
