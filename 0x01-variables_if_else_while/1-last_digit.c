9bbe7ed4a0d8dc7f443b0be93faeb7550a8b9849
Paradox <fathiaraji4@gmail.com>
2022-09-09 15:24:57 -0700

    Task 1. The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the last digit of the number stored in the variable n.
+ *
+ * Description: Using the main function
+ * this program prints the last digit of the number stored in the variable n.
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
