43733aaf3e76570e17734ffb3d385797ff413800
Paradox <fathiaraji4@gmail.com>
2022-09-09 15:45:33 -0700

    Task 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,23 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	int n;
+	int l;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l > 5)
 	{
-		putchar(ch);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	putchar('\n');
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d amd is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
 	return (0);
 }
