765106a8fe799a53173e7a7b6c9bc733685bc426
Paradox <fathiaraji4@gmail.com>
2022-09-09 17:57:45 -0700

    Task 8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  prints all the numbers of base 16 in lowercase, 
+ * main -  prints all the numbers of base 16 in lowercase,
  * followed by a new line.
  *
  * Description: using main function
@@ -11,7 +11,7 @@ int main(void)
 {
 	char c;
 
-	for( c = '0' ; c <= '9'; c++)
+	for(c = '0' ; c <= '9'; c++)
 	{
 		putchar(c);
 	}
