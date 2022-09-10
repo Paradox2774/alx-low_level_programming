a70d61155174c6a6970e033312ed01440a4fc6b2
Paradox <fathiaraji4@gmail.com>
2022-09-09 17:42:56 -0700

    Task 7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ *
+ * Description: using main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
