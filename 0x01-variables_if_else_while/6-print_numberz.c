612baee42b3fc80c07588fb69beb7ffb58b6f560
Paradox <fathiaraji4@gmail.com>
2022-09-09 18:25:53 -0700

    Task 6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main -  prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ * Description: using main function
+ *
+ * Return: 0
+ */
+int mai(void)
+{
+	int n;
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
