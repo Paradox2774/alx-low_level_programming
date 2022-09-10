4aafccd92479cdd4df639fd9bc2f142bcd59f6ac
Paradox <fathiaraji4@gmail.com>
2022-09-09 17:15:16 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ *
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		ptintf(%i, i);
+	}
+	printf('\n');
+	return (0);
+}
