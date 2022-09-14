863f2cc89763761f125561c4b921b71167998a7c
Paradox <fathiaraji4@gmail.com>
2022-09-14 06:15:36 -0700

    Task 15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - maain block
+ * Decription: prints the sum of even fibonacci numbers up to fib value
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i = 1, j = 2; total = 0;
+	int k;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total += j;
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%d\n", total);
+	return (0);
+}
