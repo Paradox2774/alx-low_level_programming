29632eacc2febe8262fc70b7418f57c3ab941585
Paradox <fathiaraji4@gmail.com>
2022-09-13 18:32:06 -0700

    Task 7. There are only 3 colors, 10 digits, and 7 notes;

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: The number in question
+ *
+ * Return: 0
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit * '0');
+
+	return (0);
+}
