3a4cc32dac9c6d96cebb06ad5f544779b89667a4
Paradox <fathiaraji4@gmail.com>
2022-09-13 19:35:26 -0700

    Task 11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural nuber from input to 98
+ *		in ordseparated by a coma followed by a space.
+ * @n: The number to begin counting at
+ * Return: number
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
