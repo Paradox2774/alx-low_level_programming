4c062e665cc73337618e1591ae1d146b7c01107e
Paradox <fathiaraji4@gmail.com>
2022-09-14 07:06:11 -0700

    Task 16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -26,7 +26,7 @@ int main(void)
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
 
-	for (i = 92; i < 99; ++i);
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
@@ -37,5 +37,6 @@ int main(void)
 	}
 
 	printf("\n");
+
 	return (0);
-}
+}	
