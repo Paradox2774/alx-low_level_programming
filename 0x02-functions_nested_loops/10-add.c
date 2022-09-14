ce02d3def43b090276eb1fc95bb27f58515c56ac
Paradox <fathiaraji4@gmail.com>
2022-09-13 19:26:57 -0700

    Task 10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,9 @@
 
 /**
  * add - Adds two integers
- * @num1: The first integer to be added 
+ * @num1: The first integer to be added
  * @num2: The second integer to be added
+ * Return: nm1 + num2
 */
 int add(int num1, int num2)
 {
