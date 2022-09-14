ae131eae971d3fd0ab289a52c6250d354dd29548
Paradox <fathiaraji4@gmail.com>
2022-09-14 05:49:41 -0700

    Task 13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - sums multiplies 0f 3 or 5
+ * Decription: using main function
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int start_num, end_num, total;
+
+	end_num = 1024;
+	total = 0;
+
+	for (start_num = 0; start_num < end_num; start_num++)
+	{
+		if ((start_num % 3 == 0) || (start_num % 5 == 0))
+		{
+			total = total + start_num;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d", total)l
+	printf('\n');
+
+	return (0);
+}
