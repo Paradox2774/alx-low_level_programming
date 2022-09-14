ae391776232f1756f248174d9401b777e7ad9646
Paradox <fathiaraji4@gmail.com>
2022-09-13 17:31:46 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
-/*
- main - prints putchar
- Return: 0
+/**
+ *main - prints putchar
+ * Description: Using main function
+ *
+ *Return: 0
 */
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
