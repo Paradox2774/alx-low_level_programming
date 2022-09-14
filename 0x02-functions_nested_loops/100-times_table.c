0b0139fd3399bb62e00708715cda7e43e0f8986f
Paradox <fathiaraji4@gmail.com>
2022-09-14 05:39:29 -0700

    Task 12. The World looks like a multiplication-table, or a mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,23 +19,29 @@ void print_times_table(int n)
 				if (j == 0)
 					_putchar(k + '0');
 				else if (k < 10 && j != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
+				}
 				else if (k >= 10 && k < 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
+				}
 				else if (k >= 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
