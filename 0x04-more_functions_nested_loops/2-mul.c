1faed7a955e154700a30664b7734fd0bdd66fa67
Paradox <fathiaraji4@gmail.com>
2022-09-15 17:05:42 -0700

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int mul(int a, int b) - multtiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a * b
+*/
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
