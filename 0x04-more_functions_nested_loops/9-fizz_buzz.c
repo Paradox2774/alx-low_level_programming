1ecf717db8e82c6b362437fd565c2d1acf450196
Paradox <fathiaraji4@gmail.com>
2022-09-15 18:56:17 -0700

    Task 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,22 +6,34 @@
 */
 int main(void)
 {
-	int i;
+	int num = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (num++ < 100)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((num % 3) == 0)
+		{
 			printf("Fizz ");
-		else if ((i % 5) == 0 && (i % 3) != 0 && i != 100)
-			printf("Buzz ");
-		else if ((i % 5) == 0 && (i % 3) == 0 != 0 && i == 100)
-			printf("Buzz");
-		else if ((i % 3) == 0 && (i % 5) == 0)
-			printf("FizzBuzz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
 		else
-			printf("%d ", i);
+		{
+			printf("%d ", num);
+		}
 	}
 	printf("\n");
-
 	return (0);
 }
