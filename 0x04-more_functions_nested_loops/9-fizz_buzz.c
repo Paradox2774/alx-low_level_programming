ba585a7d1689e08cbff5c9baea3a03d145e0bd35
Paradox <fathiaraji4@gmail.com>
2022-09-15 17:59:15 -0700

    Task 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a program that solves the 'FizzBuzz' test
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0 && (i % 3) != 0 && i == 100)
+			printf("Buzz ");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
