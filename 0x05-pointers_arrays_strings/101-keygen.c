3bfbbed45dc2de6640dddbbd6f4c3147e81d22d4
Paradox <fathiaraji4@gmail.com>
2022-09-19 18:21:09 -0700

    Task 11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - generates keygen.
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  * Return: 0 Always.
 */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+	int pass[100];
+	int i, sum, n;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	sum = 0;
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
 			break;
-		c = c + r;
-		printf("%c", r);
+		}
 	}
-	printf("%c\n", (2772 - c));
 	return (0);
 }
