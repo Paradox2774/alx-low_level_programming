150ea5041358110cf233d63eb62b4b76d97c46b2
Paradox <fathiaraji4@gmail.com>
2022-09-22 13:51:24 -0700

    Task 1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,7 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (count2 < n)
 	{
 		*(dest + count) = *(src + count2);
-		if (*src + count2) == '\0')
+		if (*(src + count2) == '\0')
 			break;
 		count++;
 		count2++;
