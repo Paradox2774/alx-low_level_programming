0733552e949118d4c6bf95fbf3ba537595aa8dd3
Paradox <fathiaraji4@gmail.com>
2022-10-12 16:45:33 -0700

    Task 0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A pointer to a function that prints a name.
- */
+  * print_name - Prints a name
+  * @name: The name to prints
+  * @f: Pointer to function
+  *
+  * Return: Nothing
+  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
